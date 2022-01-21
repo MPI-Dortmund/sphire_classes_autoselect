e2d709988a785c86bde2e8c0428a07d17c2b346b
Thorsten Wagner <twa1@posteo.de>
2022-01-21 15:03:28 +0100

    Specify upper h5py version, fix issue #6

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "Keras == 2.2.5",
         "numpy == 1.14.5",
-        "h5py >= 2.5.0",
+        "h5py >= 2.5.0, < 3.0.0",
         "scipy >= 1.3.0",
         "pillow",
         "tqdm",
